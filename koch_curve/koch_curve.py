e6eb7973521801de0d8ff435079bcab4fb7bdc6c
AlinaRyabova <115992255+AlinaRyabova@users.noreply.github.com>
2024-07-13 20:43:40 +0300

    Update koch_curve.py

--- koch_curve/koch_curve.py
+++ koch_curve/koch_curve.py
@@ -1,37 +1,42 @@
 import turtle
 
-def koch_curve(t, length, level):
-    if level == 0:
-        t.forward(length)
-    else:
-        length /= 3
-        koch_curve(t, length, level - 1)
-        t.left(60)
-        koch_curve(t, length, level - 1)
-        t.right(120)
-        koch_curve(t, length, level - 1)
-        t.left(60)
-        koch_curve(t, length, level - 1)
+def koch_curve(t, order, size):
+    """Функція для малювання кривої Коха за допомогою черепашки.
 
-def snowflake(t, length, level):
+    Args:
+        t (turtle.Turtle): Об'єкт черепашки для малювання.
+        order (int): Поточний рівень рекурсії.
+        size (float): Довжина сторони кривої на поточному рівні рекурсії.
+
+    """
+    if order == 0:
+        t.forward(size)
+    else:
+        for angle in [60, -120, 60, 0]:
+            koch_curve(t, order - 1, size / 3)
+            t.left(angle)
+
+def snowflake(t, order, size):
+    #Функція для малювання сніжинки Коха.
     for _ in range(3):
-        koch_curve(t, length, level)
+        koch_curve(t, order, size)
         t.right(120)
 
 def main():
+    #Головна функція програми для малювання сніжинки Коха.
     screen = turtle.Screen()
     screen.setup(width=800, height=800)
 
     t = turtle.Turtle()
-    t.speed(0)
+    t.speed(0)  # Налаштування швидкості черепашки (0 - максимальна швидкість)
 
-    length = 300  # початкова довжина сторін сніжинки
+    length = 300  # Початкова довжина сторін сніжинки
     
     while True:
         try:
-            level = int(input("Enter the level of recursion (0-6): "))
+            level = int(input("Введіть рівень рекурсії (0-6): "))
             if level < 0 or level > 6:
-                raise ValueError("Level must be between 0 and 6")
+                raise ValueError("Рівень має бути від 0 до 6")
             break
         except ValueError as e:
             print(e)
@@ -40,7 +45,7 @@ def main():
     t.goto(-length/2, length/3)
     t.pendown()
 
-    snowflake(t, length, level)
+    snowflake(t, level, length)
 
     turtle.done()
 
